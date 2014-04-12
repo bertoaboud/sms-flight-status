4c6c6c24a42d352716dfa1d1cc8eeba78d78f619
kate <aboud@aboud-ThinkPad-W530.(none)>
2014-04-12 02:05:28 -0400

    controller for sms finished

--- app/controllers/sendtext_controller.rb
+++ app/controllers/sendtext_controller.rb
@@ -5,9 +5,9 @@ class SendtextController < ApplicationController
 	def send_text_message
 		number_to_send_to = params[:number_to_send_to]
 	 
-		twilio_sid = "abc123"
-		twilio_token = "foobar"
-		twilio_phone_number = "6165555555"
+		twilio_sid = "PN3516502740c1f1bdfad4d56e7d7474e9"
+		twilio_token = "e7bd7f97aa4b0858d8c5bd09732d2511"
+		twilio_phone_number = "+18128644782"
 	 
 		@twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
 	 
