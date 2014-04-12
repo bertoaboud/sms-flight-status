8ca90ffa6f16fbbf35a14a77642902ecc966a47d
kate <aboud@aboud-ThinkPad-W530.(none)>
2014-04-12 02:52:58 -0400

    send sms working

--- app/controllers/sendtext_controller.rb
+++ app/controllers/sendtext_controller.rb
@@ -3,11 +3,10 @@ class SendtextController < ApplicationController
 	end
 	 
 	def send_text_message
-		number_to_send_to = params[:number_to_send_to]
-	 
-		twilio_sid = "PN3516502740c1f1bdfad4d56e7d7474e9"
+	  number_to_send_to = params[:number_to_send_to]
+		twilio_sid = "ACedfb08c8d4d265990a4ab42477e2e52a"
 		twilio_token = "e7bd7f97aa4b0858d8c5bd09732d2511"
-		twilio_phone_number = "+18128644782"
+		twilio_phone_number = "8128644782"
 	 
 		@twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
 	 
@@ -16,6 +15,8 @@ class SendtextController < ApplicationController
 			:to => number_to_send_to,
 			:body => "This is an message. It gets sent to #{number_to_send_to}"
 		)
+		render :nothing => true
 	end
  
+
 end
